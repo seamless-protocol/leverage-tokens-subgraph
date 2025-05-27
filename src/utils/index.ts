56df4de78ce20683f67bf4fa9b26198da392eae2
chad-js <chadflemies@gmail.com>
2025-05-27 12:20:18 -0400

    feat: handle redeems and equity transfers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,10 @@
-import { Address } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { Position } from "../../generated/schema";
+import { WAD_STRING } from "../constants";
+
+export function calculateCollateralRatio(totalCollateralInDebt: BigInt, totalDebt: BigInt): BigInt {
+    return totalCollateralInDebt.times(BigInt.fromString(WAD_STRING)).div(totalDebt);
+}
 
 export function getPosition(
     user: Address,
