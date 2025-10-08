405c0f83f21c8161daf891ec3d394e2a7a92bfed
chad-js <chadflemies@gmail.com>
2025-10-08 16:16:54 -0400

    feat: support v0 mint and redeem events

--- templates/8453/addresses.template.ts
+++ templates/8453/addresses.template.ts
@@ -1,1 +1,2 @@
 export const LEVERAGE_MANAGER_ADDRESS = "{{base.LeverageManager.address}}"
+export const MORPHO_CHAINLINK_ORACLE_V2_WSTETH_STETH_ADDRESS = "{{base.MorphoChainlinkOracleV2-wstETH-stETH.address}}"
