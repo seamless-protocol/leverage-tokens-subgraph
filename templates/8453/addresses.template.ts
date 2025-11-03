e0cb23948c262903924ea7cd7f3d04c5f80cf9ef
chad-js <chadflemies@gmail.com>
2025-11-03 17:35:55 -0500

    feat: support for oracle polling

--- templates/8453/addresses.template.ts
+++ templates/8453/addresses.template.ts
@@ -2,3 +2,7 @@ export const LEVERAGE_MANAGER_ADDRESS = "{{base.LeverageManager.address}}"
 
 export const MORPHO_CHAINLINK_ORACLE_V2_RLP_USDC_ADDRESS = "{{base.MorphoChainlinkOracleV2-RLP-USDC.address}}"
 export const MORPHO_CHAINLINK_ORACLE_V2_WSTETH_STETH_ADDRESS = "{{base.MorphoChainlinkOracleV2-wstETH-stETH.address}}"
+
+// TODO: Uncomment this when there are oracles set to be polled
+// export const CHAINLINK_ORACLE_POLLING_ADDRESSES = "{{base.ChainlinkOraclePolling.oracles}}".split(",")
+export const CHAINLINK_ORACLE_POLLING_ADDRESSES: string[] = []
